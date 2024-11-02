e1356b6b15675cdfb416b147f1a8dc456ebb9633
lororock <cristhianrosas159@hotmail.com>
2024-11-02 13:47:59 -0500

    add category component and iterate

--- src/app/shared/category-card/category-card.component.ts
+++ src/app/shared/category-card/category-card.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-category-card',
+  selector: 'category-card',
+  standalone: true,
+  imports: [],
   templateUrl: './category-card.component.html',
-  styleUrls: ['./category-card.component.css']
+  styleUrls: ['./category-card.component.css'],
 })
-export class CategoryCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class CategoryCardComponent {
+  @Input() icon = '';
+  @Input() text = '';
 }
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { KeywordSearchComponent } from '@/app/shared/keyword-search/keyword-search.component';
-
+import { CategoryCardComponent } from '@/app/shared/category-card/category-card.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [KeywordSearchComponent],
+  imports: [KeywordSearchComponent, CategoryCardComponent],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent{
-
+export class HomeComponent {
+  categories = [
+    { id: 1, icon: '👟', text: 'Ropa' },
+    { id: 2, icon: '⚽', text: 'Deporte' },
+    { id: 3, icon: '🎮', text: 'Juegos' },
+    { id: 4, icon: '🎁', text: 'Regalos' },
+    { id: 5, icon: '🧳', text: 'Maletas' },
+    { id: 6, icon: '🎄', text: 'Navidad' },
+    { id: 7, icon: '👓', text: 'Gafas' },
+  ];
 }
