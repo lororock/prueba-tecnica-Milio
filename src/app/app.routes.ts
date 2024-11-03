34484d4f86952955769ad6636a1595acbb473e0a
lororock <cristhianrosas159@hotmail.com>
2024-11-03 11:44:11 -0500

    page categories and add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './views/main/main.component';
 import { UserComponent } from './views/user/user.component'; 
 import { HomeComponent } from './views/home/home.component';
+import { CategoriesComponent } from './views/categories/categories.component';
 import { NotFoundComponent } from './views/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent, children: [
     { path: '', component: HomeComponent },
     { path: 'user', component: UserComponent },
-    // { path: 'categorias', component: CategoryComponent }, se puede agregar mas rutas que requieran las barras de navegacion
+    { path: 'categories', component: CategoriesComponent },
+    //se puede agregar mas rutas que requieran las barras de navegacion
   ]},
   { path: '**', component: NotFoundComponent }
 
---
+++ src/app/views/categories/categories.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories',
+  standalone: true,
+  imports: [],
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
+})
+export class CategoriesComponent  {
+
+}
