7a018665139211669bffacae72a8db8877f49e55
lororock <cristhianrosas159@hotmail.com>
2024-11-04 05:37:45 -0500

    come back perfil

--- src/app/views/user/user.component.ts
+++ src/app/views/user/user.component.ts
@@ -1,10 +1,11 @@
 import { ProfileButtonComponent } from '@/app/shared/profile-button/profile-button.component';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [ProfileButtonComponent],
+  imports: [ProfileButtonComponent, RouterLink],
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
