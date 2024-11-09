8d9db057921e91e59639f1da5ce038fd636c107f
lororock <cristhianrosas159@hotmail.com>
2024-11-09 02:07:58 -0500

    like on product card

--- src/app/shared/product-card/product-card.component.ts
+++ src/app/shared/product-card/product-card.component.ts
@@ -1,20 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '@/app/core/models/product.model';
-
 @Component({
   selector: 'product-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent{
+
+  like: boolean = false
+
   @Input() name: string = '';
   @Input() price: number = 0;
   @Input() img: string = '';
   @Input() product!: Product;
 
   @Output() openModalEvent = new EventEmitter<Product>();
+
+  addFav(){
+    this.like = !this.like
+  }
 
   handleClick() {
     this.openModalEvent.emit(this.product);
